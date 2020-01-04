b2b58f4ec0abb983e9e0b959a911e7532b602aeb
Madrog <rogers.madda@gmail.com>
2020-01-04 13:50:13 +0300

    first commit

---
+++ LearnMongo/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Entities.Core;
+
+namespace LearnMongo
+{
+    class Person : Entity
+    {
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int SiblingCount { get; set; }
+    }
+}
---
+++ LearnMongo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using MongoDB.Entities;
+
+namespace LearnMongo
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            new DB("MyDatabase", "localhost", 27017);
+
+            var lisa = new Person
+            {
+                Name = "Lisa Malfrey",
+                DateOfBirth = new DateTime(1983, 10, 11),
+                SiblingCount = 1
+            };
+
+            lisa.Save();
+
+            Console.WriteLine($"Lisa's ID: {lisa.ID}");
+
+            //var result = DB.Queryable<Person>()
+            //                .Where(p => p.DateOfBirth > new DateTime(1983, 10, 10) &&
+            //                       p.DateOfBirth < new DateTime(1983, 10, 12))
+            //                .First();
+            //Console.WriteLine($"Birthday: {result.DateOfBirth.ToLocalTime()}");
+
+            //var result = DB.Find<Person>()
+            //                .Many(p => p.SiblingCount >= 1)
+            //                .First();
+            //Console.WriteLine($"Count: {result.SiblingCount}");
+
+            Console.Read();
+        }
+    }
+}
